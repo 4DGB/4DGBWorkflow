6745637d3a26f9a55bb9b585c41d48b6fc063cad
David Honegger Rogers <dhr@lanl.gov>
2022-10-17 16:40:15 -0600

    updating test

--- testing/test_build.py
+++ testing/test_build.py
@@ -12,7 +12,14 @@ class TestCSV2Tracks(unittest.TestCase):
     def test_build(self):
         subprocess.check_output('./4DGBWorkflow template', stderr=subprocess.STDOUT, shell=True)
         subprocess.check_output('./4DGBWorkflow build 4DGB_Project', stderr=subprocess.STDOUT, shell=True)
+
         x = subprocess.check_output('wc -l 4DGB_Project/.build/lammps_0', stderr=subprocess.STDOUT, shell=True)
+        result = x.decode("utf-8")
+        result = result.strip()
+        # did the computation create the expected number of points?
+        self.assertEqual(int(result), 255)
+
+        x = subprocess.check_output('wc -l 4DGB_Project/.build/lammps_1', stderr=subprocess.STDOUT, shell=True)
         result = x.decode("utf-8")
         result = result.strip()
         # did the computation create the expected number of points?
