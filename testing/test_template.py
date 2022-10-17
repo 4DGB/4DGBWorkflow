22b406307b948679a0d755122ef6f74232ef2620
David Honegger Rogers <dhr@lanl.gov>
2022-10-17 11:35:19 -0600

    updating test

---
+++ testing/test_template.py
@@ -0,0 +1,22 @@
+import unittest
+# import os
+import subprocess
+import numpy
+import shutil
+# import filecmp
+
+class TestCSV2Tracks(unittest.TestCase):
+    checksum = "b49d3a9c1b42d804be553b1537e23a63"
+
+    def __init__(self, *args, **kwargs):
+        super(TestCSV2Tracks, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        print("Running test: {}".format(self._testMethodName))
+
+    def test_create(self):
+        # create the destination directory; copy files
+        x = subprocess.check_output(['./4DGBWorkflow template'])
+        print("result is {}".format(x))
+        self.assertEqual(x, self.checksum )
+
