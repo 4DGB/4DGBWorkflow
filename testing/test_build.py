d54c8943006dc4268eb90f006ff85165f4183afd
David Honegger Rogers <dhr@lanl.gov>
2022-10-17 13:51:10 -0600

    fixing test

--- testing/test_build.py
+++ testing/test_build.py
@@ -12,6 +12,7 @@ class TestCSV2Tracks(unittest.TestCase):
 
     def test_build(self):
         # create the destination directory; copy files
+        subprocess.check_output('./4DGBWorkflow template', stderr=subprocess.STDOUT, shell=True)
         subprocess.check_output('./4DGBWorkflow build 4DGB_Project', stderr=subprocess.STDOUT, shell=True)
         x = subprocess.check_output('checksumdir -a md5 4DGB_Project', stderr=subprocess.STDOUT, shell=True)
         result = x.decode("utf-8")
