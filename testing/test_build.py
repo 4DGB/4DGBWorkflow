5f71dc67a11e9a9877a0d55a9b9114d95ece14eb
David Honegger Rogers <dhr@lanl.gov>
2022-10-17 19:03:36 -0600

    test fix

--- testing/test_build.py
+++ testing/test_build.py
@@ -33,7 +33,7 @@ class TestCSV2Tracks(unittest.TestCase):
                     ["acc0e566818f2e5479446a0b8d3616ee", "4DGB_Project/.build/tracks/H3K27ac/track.npz"]
                 ]
         for test in tests:
-            x = subprocess.check_output("md5 -q {}".filename(test[1]), stderr=subprocess.STDOUT, shell=True)
+            x = subprocess.check_output("md5 -q {}".format(test[1]), stderr=subprocess.STDOUT, shell=True)
             result = x.decode("utf-8")
             result = result.strip()
             self.assertEqual(result, test[0])
